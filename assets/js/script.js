c4195a726c68471e4a3cf6374e58bac4326ef1c4
Ventricule <pierretandille@gmail.com>
2016-03-22 14:11:55 +0100

    searchbar animée

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,8 @@ $(document).ready( function () {
 	----------------------------------------- */
 	$("#menu-second").perfectScrollbar(); 
 	
+	
+	
 	/* Liste des membres : Couleur
 	----------------------------------------- */
 
@@ -61,177 +63,226 @@ $(document).ready( function () {
 	
 	/* Carte des membres Mapbox GL 
 	----------------------------------------- */
-	mapboxgl.accessToken = 'pk.eyJ1IjoicGllcnJlcGllcnJlcGllcnJlIiwiYSI6IkdXdE5CRFEifQ.3zLbKVYfHituW8BVU-bl5g';
-	
-	/* Init */
-	var map = new mapboxgl.Map({
-			container: 'map-membres', // container id
-			style: 'mapbox://styles/pierrepierrepierre/ciku1r9cd00a3bdklgumavnex', //stylesheet location,
-		  sprite: "mapbox://sprites/mapbox/satellite-v8",
-			center: [2.53, 46.8], // starting position
-			zoom: 5, // starting zoom
-			minZoom: 4,
-			scrollZoom: false,
-			attributionControl: false
-	})
-	.fitBounds( [[9.55932, 51.089062],[-5.1406, 41.33374]] )
-	.addControl(new mapboxgl.Navigation({position: 'top-right'}));;
-	
-	/* Rotate */
-	map.once('moveend', function() {
-		map.setBearing(90);
-	});
-	
-	/* Filters */
-	var filterGroup = document.getElementById('filter-group');
-	
-	map.on('load', function() {
-    // Add marker data as a new GeoJSON source.
-    map.addSource("markers", {
-        "type": "geojson",
-        "data": markers
-    });
-		
-		map.addLayer({
-				"id": 'markers',
-				"type": "circle",
-				"source": "markers",
-				"interactive": true,
-				"paint": {
-						"circle-radius": 10,
-						"circle-color": '#0000E6'
-				}
+	if($('#map-membres').size()) {
+		mapboxgl.accessToken = 'pk.eyJ1IjoicGllcnJlcGllcnJlcGllcnJlIiwiYSI6IkdXdE5CRFEifQ.3zLbKVYfHituW8BVU-bl5g';
+
+		/* Init */
+		var map = new mapboxgl.Map({
+				container: 'map-membres', // container id
+				style: 'mapbox://styles/pierrepierrepierre/ciku1r9cd00a3bdklgumavnex', //stylesheet location,
+				sprite: "mapbox://sprites/mapbox/satellite-v8",
+				center: [2.53, 46.8], // starting position
+				zoom: 5, // starting zoom
+				minZoom: 4,
+				scrollZoom: false,
+				attributionControl: false
+		})
+		.fitBounds( [[9.55932, 51.089062],[-5.1406, 41.33374]] )
+		.addControl(new mapboxgl.Navigation({position: 'top-right'}));;
+
+		/* Rotate */
+		map.once('moveend', function() {
+			map.setBearing(90);
 		});
-		
-		markers.features.forEach(function(feature) {
-			var symbol = feature.properties['marker-symbol'];
-			var layerID = 'poi-' + symbol;
-			var activites = feature.properties;
-			addCheckbox('all');
-			$.each(activites, function(key, value) {
-					if(!key.indexOf('activite-')) {
-						addCheckbox(key);
+
+		/* Filters */
+		var filterGroup = document.getElementById('filter-group');
+
+		map.on('load', function() {
+			// Add marker data as a new GeoJSON source.
+			map.addSource("markers", {
+					"type": "geojson",
+					"data": markers
+			});
+
+			map.addLayer({
+					"id": 'markers',
+					"type": "circle",
+					"source": "markers",
+					"interactive": true,
+					"paint": {
+							"circle-radius": 10,
+							"circle-color": '#0000E6'
 					}
 			});
-			
-			function addCheckbox(id) {
-				if ( ! $(filterGroup).children('#'+id).length ) {
 
-					// Add checkbox and label elements for the layer.
-					var input = document.createElement('input');
-					input.type = 'checkbox';
-					input.id = id;
-					input.checked = id == 'all' ? true : false;
-					filterGroup.appendChild(input);
-
-					var label = document.createElement('label');
-					label.setAttribute('for', id);
-					label.innerHTML = '<span>' + id2title(id) + '</span>';
-					filterGroup.appendChild(label);
-
-					// When the checkbox changes, update the visibility of the layer.
-					input.addEventListener('change', function(e) {
-						$(filterGroup).children('input').not(e.target).prop("checked", false);
-						if(e.target.checked) {
-							if (id == 'all') {
-								map.setFilter("markers", ['any','all']);
-								listSetFilter('all');
-							} else {
-								map.setFilter("markers", ['==', id, true]);
-								listSetFilter(id);
-							}
-						} else {
-							map.setFilter("markers", ['none','all']);
+			markers.features.forEach(function(feature) {
+				var symbol = feature.properties['marker-symbol'];
+				var layerID = 'poi-' + symbol;
+				var activites = feature.properties;
+				addCheckbox('all');
+				$.each(activites, function(key, value) {
+						if(!key.indexOf('activite-')) {
+							addCheckbox(key);
 						}
+				});
 
-					});
+				function addCheckbox(id) {
+					if ( ! $(filterGroup).children('#'+id).length ) {
+
+						// Add checkbox and label elements for the layer.
+						var input = document.createElement('input');
+						input.type = 'checkbox';
+						input.id = id;
+						input.checked = id == 'all' ? true : false;
+						filterGroup.appendChild(input);
+
+						var label = document.createElement('label');
+						label.setAttribute('for', id);
+						label.innerHTML = '<span>' + id2title(id) + '</span>';
+						filterGroup.appendChild(label);
+
+						// When the checkbox changes, update the visibility of the layer.
+						input.addEventListener('change', function(e) {
+							$(filterGroup).children('input').not(e.target).prop("checked", false);
+							if(e.target.checked) {
+								if (id == 'all') {
+									map.setFilter("markers", ['any','all']);
+									listSetFilter('all');
+								} else {
+									map.setFilter("markers", ['==', id, true]);
+									listSetFilter(id);
+								}
+							} else {
+								map.setFilter("markers", ['none','all']);
+							}
+
+						});
+					}
 				}
-			}
-			function listSetFilter(id) {
-				id = id.replace('activite-', ''); 
-				if(id=='all') {
-					$("#liste-membres li").show();
-				} else {
-					$("#liste-membres li").hide();
-					$("#liste-membres li[data-activites~='"+id+"']").show();
-					console.log(id);
+				function listSetFilter(id) {
+					id = id.replace('activite-', ''); 
+					if(id=='all') {
+						$("#liste-membres li").show();
+					} else {
+						$("#liste-membres li").hide();
+						$("#liste-membres li[data-activites~='"+id+"']").show();
+						console.log(id);
+					}
 				}
-			}
-			function id2title(id) {
-				id = id.replace('activite-', ''); 
-				switch (id) {
-					case 'diffusion':
-						return 'Lieux de diffusion';
-						break;
-					case 'residence':
-						return 'Lieux de résidence';
-						break;
-					case 'chapiteau':
-						return 'Espaces chapiteau';
-						break;
-					case 'polenationnal':
-						return 'Pôles nationnaux';
-						break;
-					case 'festival':
-						return 'Festivals';
-						break;
-					case 'formation':
-						return 'Formations professionnelles';
-						break;
-					case 'all':
-						return 'Tous les membres';
-						break;
-					default:
-						return id;
-						break;
+				function id2title(id) {
+					id = id.replace('activite-', ''); 
+					switch (id) {
+						case 'diffusion':
+							return 'Lieux de diffusion';
+							break;
+						case 'residence':
+							return 'Lieux de résidence';
+							break;
+						case 'chapiteau':
+							return 'Espaces chapiteau';
+							break;
+						case 'polenationnal':
+							return 'Pôles nationnaux';
+							break;
+						case 'festival':
+							return 'Festivals';
+							break;
+						case 'formation':
+							return 'Formations professionnelles';
+							break;
+						case 'all':
+							return 'Tous les membres';
+							break;
+						default:
+							return id;
+							break;
+					}
 				}
-			}
-			
-    });
-		
-		var popup = new mapboxgl.Popup();
-	
-		// When a click event occurs near a marker icon, open a popup at the location of
-		// the feature, with description HTML from its properties.
-		map.on('click', function (e) {
-			map.featuresAt(e.point, {
-					radius: 10, // Half the marker size (15px).
-					includeGeometry: true,
-					layer: 'markers'
-			}, function (err, features) {
 
-					if (err || !features.length) {
-							popup.remove();
-							return;
-					}   
-
-					var feature = features[0];
-				
-				console.log(feature.properties);
-
-					// Popuplate the popup and set its coordinates
-					// based on the feature found.
-					popup.setLngLat(feature.geometry.coordinates)
-							.setHTML( $('#liste-membres ul li').filter('[data-uid='+feature.properties.uid+']').html() )
-							.addTo(map);
 			});
-		});
 
-		// Use the same approach as above to indicate that the symbols are clickable
-		// by changing the cursor style to 'pointer'.
-		map.on('mousemove', function (e) {
+			var popup = new mapboxgl.Popup();
+
+			// When a click event occurs near a marker icon, open a popup at the location of
+			// the feature, with description HTML from its properties.
+			map.on('click', function (e) {
 				map.featuresAt(e.point, {
 						radius: 10, // Half the marker size (15px).
+						includeGeometry: true,
 						layer: 'markers'
 				}, function (err, features) {
-						map.getCanvas().style.cursor = (!err && features.length) ? 'pointer' : '';
+
+						if (err || !features.length) {
+								popup.remove();
+								return;
+						}   
+
+						var feature = features[0];
+
+					console.log(feature.properties);
+
+						// Popuplate the popup and set its coordinates
+						// based on the feature found.
+						popup.setLngLat(feature.geometry.coordinates)
+								.setHTML( $('#liste-membres ul li').filter('[data-uid='+feature.properties.uid+']').html() )
+								.addTo(map);
 				});
+			});
+
+			// Use the same approach as above to indicate that the symbols are clickable
+			// by changing the cursor style to 'pointer'.
+			map.on('mousemove', function (e) {
+					map.featuresAt(e.point, {
+							radius: 10, // Half the marker size (15px).
+							layer: 'markers'
+					}, function (err, features) {
+							map.getCanvas().style.cursor = (!err && features.length) ? 'pointer' : '';
+					});
+			});
+
 		});
+	}
+	
+	
+	
+	/* Folder : SearchBar
+	----------------------------------------- */
+	$('#searchbar .btn-more').click(function(){
+		$('#searchbar').toggleClass('full');
+		$('#searchbar').addClass('transition');
+		setTimeout(function() {
+			$('#searchbar').removeClass('transition');
+		}, 300)
 		
 	});
 
 	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*!
  * imagesLoaded PACKAGED v4.1.0
--- site/snippets/modules/searchbar.php
+++ site/snippets/modules/searchbar.php
@@ -1,24 +1,37 @@
 <div id="searchbar">
 	<form>
 		<input type="search" name="q" value="" placeholder="Rechercher">
-		<select name="author">
-			<option label="Auteur">Auteur</option>
-			<?php foreach (page('ressources')->index()->pluck('author', ',', true) as $author) : ?>
-				<option value="<?= html($author) ?>"><?= html($author) ?></option>
-			<?php endforeach ?>
-		</select>
-		<select name="member">
-			<option label="Membre">Membre</option>
-				<?php foreach (page('ressources')->index()->pluck('member', ',', true) as $member) : ?>
-			<option value="<?= $member ?>"><?= $member ?></option>
-			<?php endforeach ?>
-		</select>
-		<select name="year">
-			<option value="">Année</option>
-			<?php for($i = 2009 ; $i <= date("Y") ; $i++) : ?>
-				<option value="<?= $i ?>"><?= $i ?></option>
-			<?php endfor ?>
-		</select>
+		<div class="btn-more"><span>+</span> <span>Recherche avancée</span></div>
+		<div class="more">
+			<div class="here">
+				<input type="checkbox" name="here" id="here" /> <label for="here">Dans ce<br>dossier</label>
+			</div>
+			<select name="tags">
+				<option label="Tags">Mot-clé</option>
+					<?php foreach (page('ressources')->index()->pluck('tags', ',', true) as $tags) : ?>
+				<option value="<?= $tags ?>"><?= $tags ?></option>
+				<?php endforeach ?>
+			</select>
+			<select name="author">
+				<option label="Auteur">Auteur</option>
+				<?php foreach (page('ressources')->index()->pluck('author', ',', true) as $author) : ?>
+					<option value="<?= html($author) ?>"><?= html($author) ?></option>
+				<?php endforeach ?>
+			</select>
+			<select name="member">
+				<option label="Membre">Membre</option>
+					<?php foreach (page('ressources')->index()->pluck('member', ',', true) as $member) : ?>
+				<option value="<?= $member ?>"><?= $member ?></option>
+				<?php endforeach ?>
+			</select>
+			<select name="year">
+				<option value="">Année</option>
+				<?php for($i = 2009 ; $i <= date("Y") ; $i++) : ?>
+					<option value="<?= $i ?>"><?= $i ?></option>
+				<?php endfor ?>
+			</select>
+		</div>
+		
 		<input type="submit" value=">">
 	</form>
 </div>
